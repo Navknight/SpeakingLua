901e316ffbeec496171fca2960308d320ae8b0a3
Hraelzyr <54801883+Hraelzyr@users.noreply.github.com>
2022-11-11 14:44:58 +0530

    Update smt.py
    Seems to be done; will write a tester in some time. Contact if you see bugs

--- smt.py
+++ smt.py
@@ -1,6 +1,9 @@
 import lexer as lx
 import ast
 
+def bool(expr):
+    return expr!=lx.TokenType.NIL && expr!=False
+
 class Semantiff:
     def __init__(self, parser, code):
         self.parser=parser
@@ -14,36 +17,62 @@ class Semantiff:
         del symtab[varname]
         
     def evaluate(node):
+        if node==None: #3
+            return lx.TokenType.NIL
+        
         if type(node)==ast.Num:
+            return node.value
+        
+        if type(node)==ast.BoolVal:
             return node.value
         
         elif type(node)==ast.Var: #RHS variable
             if symtab.has_key(node.value): #The 'value' is the variable name here
                 return symtab[node.value]
-            else: return lx.NIL
+            else: return lx.TokenType.NIL
         
         elif type(node)==ast.VarDecl:
             return node.value
         
         elif type(node)==ast.Assign:
-            symtab[node.left.value]=self.evaluate(node.right)
+            self.create_variable(node.left.value, self.evaluate(node.right))
             
-            if symtab[node.left.value]==lx.NIL: #Clear unnecessary space
+            if symtab[node.left.value]==lx.TokenType.NIL: #Clear unnecessary space
                 self.flush(node.left.value)
-            return lx.NIL
+            return lx.TokenType.NIL
         
         elif type(node)==ast.UnaryOp:
             if node.token==lx.TokenType.PLUS:
                 return self.evaluate(node.expr)
             elif node.token==lx.TokenType.MINUS:
                 return -self.evaulate(node.expr)
+            if node.token==lx.TokenType.NOT:
+                return not bool(self.evaluate(node.expr))
             else:
                 raise Exception("Unrecognised unary operator: "+node)
                 
-        
+                
+        #Binary operation
         elif type(node)==ast.BinOp:
-            if node.right.value==lx.NIL or node.right.value==lx.NIL:
-                return lx.NIL
+            
+            # SCC Booleans
+            if node.token==lx.TokenType.OR:
+                cond = self.evaluate(node.left)
+                if bool(cond):
+                    return cond
+                else:
+                    return self.evaluate(node.right)            
+            if node.token==lx.TokenType.AND:
+                cond = self.evaluate(node.left)
+                if bool(cond):
+                    return cond
+                else:
+                    return self.evaluate(node.right)
+                
+             
+            #Arithmetic expressions
+            if node.right.value==lx.TokenType.NIL or node.right.value==lx.TokenType.NIL:
+                return lx.TokenType.NIL
             
             if node.token==lx.TokenType.EXP:
                 return self.evaluate(node.left)**self.evaluate(node.right)
@@ -61,16 +90,25 @@ class Semantiff:
             else:
                 raise Exception("Unrecognised binary operator: "+node)
                 
-                
+         
+        #While loop
         elif type(node)==ast.While:
             a = 0
             while self.evaluate(node.test):
                 a=self.evaluate(node.body)
             return a
         
+        
+        #If-elif-else ladder
+        elif type(node)==ast.Else:
+            return self.evaluate(node.body)
+        
         elif type(node)==ast.If:
             if self.evaluate(node.test):
                 return self.evaluate(node.body)
+            else:
+                return self.evaluate(node.alt)
             
+        # Find weird tokens    
         else:
             raise Exception("Unexpected token error: "+node)
